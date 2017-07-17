d6842fcdff5c0e96d3e527d97231084d4036c3e7
Sebastian Volland <seb@baunz.net>
2017-07-17 17:27:31 +0200

    pull: Don't normalize the input text on creatable
    https://github.com/JedWatson/react-select/pull/1329/commits/53af1bd79899525f65a5a68ae36adbeb4a46cbff

--- src/Async.js
+++ src/Async.js
@@ -80,16 +80,16 @@ export default class Async extends Component {
 		this.setState({ options: [] });
 	}
 
-	loadOptions (inputValue) {
+	loadOptions (inputValue, normalizedInputValue=inputValue) {
 		const { loadOptions } = this.props;
 		const cache = this._cache;
 
 		if (
 			cache &&
-			cache.hasOwnProperty(inputValue)
+			cache.hasOwnProperty(normalizedInputValue)
 		) {
 			this.setState({
-				options: cache[inputValue]
+				options: cache[normalizedInputValue]
 			});
 
 			return;
@@ -102,7 +102,7 @@ export default class Async extends Component {
 				const options = data && data.options || [];
 
 				if (cache) {
-					cache[inputValue] = options;
+					cache[normalizedInputValue] = options;
 				}
 
 				this.setState({
@@ -115,7 +115,7 @@ export default class Async extends Component {
 		// Ignore all but the most recent request
 		this._callback = callback;
 
-		const promise = loadOptions(inputValue, callback);
+		const promise = loadOptions(normalizedInputValue, callback);
 		if (promise) {
 			promise.then(
 				(data) => callback(null, data),
@@ -135,24 +135,21 @@ export default class Async extends Component {
 
 	_onInputChange (inputValue) {
 		const { ignoreAccents, ignoreCase, onInputChange } = this.props;
-		let transformedInputValue = inputValue;
+    var normalizedInputValue = inputValue;
 
 		if (ignoreAccents) {
-			transformedInputValue = stripDiacritics(transformedInputValue);
+			normalizedInputValue = stripDiacritics(normalizedInputValue);
 		}
 
 		if (ignoreCase) {
-			transformedInputValue = transformedInputValue.toLowerCase();
+			normalizedInputValue = normalizedInputValue.toLowerCase();
 		}
 
 		if (onInputChange) {
 			onInputChange(transformedInputValue);
 		}
 
-		this.loadOptions(transformedInputValue);
-
-		// Return the original input value to avoid modifying the user's view of the input while typing.
-		return inputValue;
+		return this.loadOptions(inputValue, normalizedInputValue);
 	}
 
 	inputValue() {
@@ -175,10 +172,6 @@ export default class Async extends Component {
 			return noResultsText;
 		}
 		return searchPromptText;
-	}
-
-	focus () {
-		this.select.focus();
 	}
 
 	render () {
